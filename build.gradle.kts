5a213b6e1439559320ca65ca05e838819a016925
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-01-12 10:31:42 -0800

    jackson compat with zenith >3.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,11 @@ repositories {
 dependencies {
     zenithProxy("com.zenith:ZenithProxy:$mc-SNAPSHOT")
     shade("io.javalin:javalin:6.7.0")
+
+    // todo: remove when javalin updates to jackson 3
+    shade("com.fasterxml.jackson.core:jackson-core:2.20.1")
+    shade("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+    shade("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.1")
 }
 
 tasks {
@@ -39,8 +44,8 @@ tasks {
         exclude("META-INF/maven/**")
         // todo: transform service files? seems to work fine without them for now
         dependencies {
-            exclude(dependency("org.slf4j::"))
-            exclude(dependency("org.jetbrains:annotations:"))
+            exclude(dependency("org.slf4j:.*:.*"))
+            exclude(dependency("org.jetbrains:annotations:.*:.*"))
         }
     }
 }
