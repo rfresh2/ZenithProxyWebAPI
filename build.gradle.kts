9c39c58192f7f1f6cd7ae38deab385dbfa52d42c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-02-24 21:27:57 -0800

    update to javalin 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     zenithProxy("com.zenith:ZenithProxy:$mc-SNAPSHOT")
-    shade("io.javalin:javalin:6.7.0")
+    shade("io.javalin:javalin:7.0.0")
 
     // todo: remove when javalin updates to jackson 3
     shade("com.fasterxml.jackson.core:jackson-core:2.20.1")
--- src/main/java/dev/zenith/web/api/WebServer.java
+++ src/main/java/dev/zenith/web/api/WebServer.java
@@ -19,6 +19,8 @@ import java.util.concurrent.TimeUnit;
 
 import static dev.zenith.web.WebApiPlugin.LOG;
 import static dev.zenith.web.WebApiPlugin.PLUGIN_CONFIG;
+import static io.javalin.apibuilder.ApiBuilder.beforeMatched;
+import static io.javalin.apibuilder.ApiBuilder.post;
 
 public class WebServer {
     private Javalin server;
@@ -58,55 +60,57 @@ public class WebServer {
                 var objectMapper = JavalinJackson.defaultMapper()
                         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                 config.jsonMapper(new JavalinJackson(objectMapper, false));
-            })
-            .beforeMatched(ctx -> {
-                if (PLUGIN_CONFIG.rateLimiter) {
-                    String ip = ctx.ip();
-                    synchronized (this) {
-                        int reqCount = rateLimitCache.get(ip, () -> 0);
-                        rateLimitCache.put(ip, reqCount + 1);
-                        if (reqCount >= PLUGIN_CONFIG.rateLimitRequestsPerMinute) {
-                            ctx.status(429);
-                            ctx.json(new AuthErrorResponse("Rate limit exceeded"));
-                            ctx.skipRemainingHandlers();
-                            LOG.warn("Rate limit exceeded for IP: {}", ip);
-                            return;
+                config.routes.apiBuilder(() -> {
+                    beforeMatched(ctx -> {
+                        if (PLUGIN_CONFIG.rateLimiter) {
+                            String ip = ctx.ip();
+                            synchronized (this) {
+                                int reqCount = rateLimitCache.get(ip, () -> 0);
+                                rateLimitCache.put(ip, reqCount + 1);
+                                if (reqCount >= PLUGIN_CONFIG.rateLimitRequestsPerMinute) {
+                                    ctx.status(429);
+                                    ctx.json(new AuthErrorResponse("Rate limit exceeded"));
+                                    ctx.skipRemainingHandlers();
+                                    LOG.warn("Rate limit exceeded for IP: {}", ip);
+                                    return;
+                                }
+                            }
                         }
-                    }
-                }
-                var authHeaderValue = ctx.header("Authorization");
-                if (authHeaderValue != null) {
-                    var expectedHeaderValue = PLUGIN_CONFIG.authToken;
-                    if (authHeaderValue.equals(expectedHeaderValue)) {
-                        // ok
-                        return;
-                    }
-                }
-                String reason = authHeaderValue == null
-                    ? "Authorization header missing"
-                    : "Invalid auth token";
-                ctx.json(new AuthErrorResponse(reason));
-                ctx.status(401);
-                ctx.skipRemainingHandlers();
-                LOG.warn("Denied request from {}: {}", ctx.ip(), reason);
-            })
-            .post("/command", ctx -> {
-                var req = ctx.bodyAsClass(CommandRequest.class);
-                var command = req.command();
-                var context = CommandContext.create(command, WebAPICommandSource.INSTANCE);
-                LOG.info("{} executed command: {}", ctx.ip(), command);
-                Globals.COMMAND.execute(context);
-                context.getSource().logEmbed(context, context.getEmbed());
-                String embedResponse = null;
-                String embedResponseComponent = null;
-                List<String> multiLineResponse = context.getMultiLineOutput();
-                if (context.getEmbed().isTitlePresent()) {
-                    var embedComponent = EmbedSerializer.serialize(context.getEmbed());
-                    embedResponse = ComponentSerializer.serializePlain(embedComponent);
-                    embedResponseComponent = ComponentSerializer.serializeJson(embedComponent);
-                }
-                ctx.json(new CommandResponse(embedResponse, embedResponseComponent, multiLineResponse));
-                ctx.status(200);
+                        var authHeaderValue = ctx.header("Authorization");
+                        if (authHeaderValue != null) {
+                            var expectedHeaderValue = PLUGIN_CONFIG.authToken;
+                            if (authHeaderValue.equals(expectedHeaderValue)) {
+                                // ok
+                                return;
+                            }
+                        }
+                        String reason = authHeaderValue == null
+                            ? "Authorization header missing"
+                            : "Invalid auth token";
+                        ctx.json(new AuthErrorResponse(reason));
+                        ctx.status(401);
+                        ctx.skipRemainingHandlers();
+                        LOG.warn("Denied request from {}: {}", ctx.ip(), reason);
+                    });
+                    post("/command", ctx -> {
+                        var req = ctx.bodyAsClass(CommandRequest.class);
+                        var command = req.command();
+                        var context = CommandContext.create(command, WebAPICommandSource.INSTANCE);
+                        LOG.info("{} executed command: {}", ctx.ip(), command);
+                        Globals.COMMAND.execute(context);
+                        context.getSource().logEmbed(context, context.getEmbed());
+                        String embedResponse = null;
+                        String embedResponseComponent = null;
+                        List<String> multiLineResponse = context.getMultiLineOutput();
+                        if (context.getEmbed().isTitlePresent()) {
+                            var embedComponent = EmbedSerializer.serialize(context.getEmbed());
+                            embedResponse = ComponentSerializer.serializePlain(embedComponent);
+                            embedResponseComponent = ComponentSerializer.serializeJson(embedComponent);
+                        }
+                        ctx.json(new CommandResponse(embedResponse, embedResponseComponent, multiLineResponse));
+                        ctx.status(200);
+                    });
+                });
             });
     }
 }
